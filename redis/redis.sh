d174e442fa002168a6ab00d33ee7c61b9c89fd65
Hanaasagi <ambiguous404@gmail.com>
2017-08-05 14:34:29 +0900

    add redis supported

---
+++ redis/redis.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+####################
+# utility function
+####################
+
+check_non_empty() {
+  if [[ "$1" == "" ]]; then
+    echo "ERROR: specify $2"
+    exit -1
+  fi
+}
+
+
+check_exec_success() {
+  if [[ "$1" != "0" ]]; then
+    echo "ERROR: $2 failed"
+    echo "$3"
+    exit -1
+  fi
+}
+
+####################
+#
+####################
+
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HOSTADDR="$(ip route get 1 | awk '{print $NF;exit}')"
+REDISIMAGE="redis:alpine"
+REDISDATA="/data/redis/data"
+
+
+update_images() {
+  docker pull ${REDISIMAGE}
+  check_exec_success "$?" "pulling 'redis' image"
+}
+
+
+start() {
+  docker kill  redis 2>/dev/null
+  docker rm -v redis 2>/dev/null
+
+  docker run -d --name redis \
+    -v ${REDISDATA}:/data \
+    --net=host \
+    --log-opt max-size=10m \
+    --log-opt max-file=9 \
+    ${REDISIMAGE}
+
+  check_exec_success "$?" "start redis container"
+}
+
+
+stop() {
+  docker stop redis 2>/dev/null
+  docker rm -v redis 2>/dev/null
+}
+
+destroy() {
+  stop
+  rm -rf ${REDISDATA}
+}
+
+
+####################
+# start of script
+####################
+
+Action=$1
+
+shift
+
+case "$Action" in
+  start)   start    ;;
+  stop )   stop     ;;
+  destroy) destroy  ;;
+  *)
+    echo "Usage: start | stop | destroy";;
+esac
+
+exit 0
