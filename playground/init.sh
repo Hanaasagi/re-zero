b58ec3f61ad2c27068c1186cf891207bd602a02c
Hanaasagi <ambiguous404@gmail.com>
2018-06-05 19:37:07 +0800

    Update init.sh

--- playground/init.sh
+++ playground/init.sh
@@ -52,15 +52,24 @@ echo 3 > /proc/sys/net/ipv4/tcp_fastopen
 
 
 ##################
-# Oh My ZSH
-##################
-apt-get install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-
-##################
 # Clean
 ##################
 apt-get autoremove -q -y        && \
     apt-get autoclean -q -y     && \
     rm -rf /var/lib/apt/lists/*
+
+
+##################
+# Oh My Tmux
+##################
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+echo "set -g prefix C-'\'" >> .tmux.conf.local
+
+
+##################
+# Oh My ZSH
+##################
+apt-get install zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
