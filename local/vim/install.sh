70b07a3480a2ebcaf94e506406ad84549fefee7f
Hanaasagi <ambiguous404@gmail.com>
2019-06-23 20:58:58 +0900

    add shebang

--- local/vim/install.sh
+++ local/vim/install.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+set -x
+
+
 git clone https://github.com/Hanaasagi/meido-vim.git
 cd meido-vim/
 ./meido.sh install
--- local/zsh/install.sh
+++ local/zsh/install.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+set -x
+
+
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # install oh-my-zsh
