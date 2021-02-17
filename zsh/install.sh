c71ed4cceb569a71e7c614ef34b270e0ef11f630
Hanaasagi <ambiguous404@gmail.com>
2021-02-17 13:36:22 +0000

    feat(zsh): add kubetail installation

--- zsh/install.sh
+++ zsh/install.sh
@@ -11,6 +11,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # clone plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/johanhaleby/kubetail.git t ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/kubetail
 
 cp ~/.zshrc ~/.zshrc.`date +%Y%m%d`
 cp ${CUR_DIR}/.zshrc ~/.zshrc
