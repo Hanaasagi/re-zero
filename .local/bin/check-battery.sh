27f781c25ef2801527483a8179ca6e238d97053c
Hanaasagi <ambiguous404@gmail.com>
2021-12-12 15:08:12 +0800

    fix(bin): add file lock for check-battery script

--- .local/bin/check-battery.sh
+++ .local/bin/check-battery.sh
@@ -6,6 +6,15 @@ SUPPRESSION_SECONDS=1800
 LOW_BATTERY=35
 HIGH_BATTERY=95
 LAST_TRIGGERED_AT=0
+LOCKFILE="/tmp/check-battery.lock"
+if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
+    echo "already running"
+    exit
+fi
+
+# make sure the lockfile is removed when we exit and then claim it
+trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+echo $$ > ${LOCKFILE}
 
 while true; do
 	battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)')
@@ -21,3 +30,6 @@ while true; do
 	fi
 	sleep $INTERVAL
 done
+
+rm -f ${LOCKFILE}
+
