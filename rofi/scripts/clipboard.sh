4205ba1be344c3349054219b624b1153f9289e11
Hanaasagi <ambiguous404@gmail.com>
2021-12-13 21:57:01 +0800

    feat(rofi): add clipboard manager

---
+++ rofi/scripts/clipboard.sh
@@ -0,0 +1,8 @@
+rofi \
+-kb-accept-entry "Return,Control+m,KP_Enter" \
+-kb-row-down "Down,Control+n,Control+j" \
+-kb-remove-to-eol "" \
+-kb-row-up "Up,Control+p,Control+k" \
+-modi "clipboard:greenclip print" \
+-show clipboard \
+-run-command '{cmd}'
